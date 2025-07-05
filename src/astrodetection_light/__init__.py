ec7db7c07f89852397a53e2e7d3b61aacbc799a7
savaij <sdrogo.lover@gmail.com>
2025-07-05 17:21:11 +0200

    Adding new specifics

--- src/astrodetection_light/__init__.py
+++ src/astrodetection_light/__init__.py
@@ -1,4 +1,4 @@
 from .faissd3lta import semantic_faiss, prepare_input_data
-from .utils import compute_bot_likelihood_metrics
+from .utils import *
 
 __all__ = ['semantic_faiss','compute_bot_likelihood_metrics', 'prepare_input_data']
