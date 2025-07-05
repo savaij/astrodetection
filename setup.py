45f95be351a02ba2f67ca4a59a51931dc086625a
savaij <sdrogo.lover@gmail.com>
2025-07-05 10:57:30 +0200

    fixing stuff

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='D3lta',
+    version='0.1.0',
+    description='Una libreria per analisi di dati Delta',
+    author='Il tuo nome',
+    author_email='tuo@email.com',
+    url='https://github.com/savaij/D3lta',
+    packages=find_packages(),
+    python_requires='>=3.7',
+)
