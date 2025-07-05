d2bef0452f5382ad34998f87ce0317b8140ecc7c
savaij <sdrogo.lover@gmail.com>
2025-07-05 12:35:42 +0200

    renaming folders

---
+++ astrodetection_light/__init__.py
@@ -0,0 +1,4 @@
+from .faissd3lta import semantic_faiss
+import utilities
+
+__all__ = ['semantic_faiss', 'utilities']
--- d3lta/__init__.py
+++
@@ -1,4 +0,0 @@
-from .faissd3lta import semantic_faiss
-import astro_indices
-
-__all__ = ['semantic_faiss', 'astro_indices']
