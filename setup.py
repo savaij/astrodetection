8d2c59f433a838c1d742cea5d8edb5a2d9634c70
savaij <sdrogo.lover@gmail.com>
2025-07-05 12:27:12 +0200

    renaming stuff

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='D3lta',
+    name='astrodetection',
     version='0.1.0',
-    description='Una libreria per analisi di dati Delta',
+    description='Una libreria per analisi di dati',
     author='Il tuo nome',
     author_email='tuo@email.com',
-    url='https://github.com/savaij/D3lta',
+    url='https://github.com/savaij/astrodetection',
     packages=find_packages(),
     python_requires='>=3.7',
 )
