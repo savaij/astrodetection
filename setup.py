2c3e1c1a281c3d4a1ea1f1a643889b1bf4fe1e34
savaij <sdrogo.lover@gmail.com>
2026-04-13 17:06:22 +0200

    adding pip-setup info

--- setup.py
+++ setup.py
@@ -3,11 +3,44 @@ from setuptools import setup, find_packages
 setup(
     name='astrodetection',
     version='0.1.2',
-    description='Una libreria per analisi di dati',
-    author='Il tuo nome',
-    author_email='tuo@email.com',
+    description='A Python library for detecting astroturfing (coordinated inauthentic behavior) in social media posts.',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    author='YOUR NAME',
+    author_email='YOUR@EMAIL.COM',
     url='https://github.com/savaij/astrodetection',
+    project_urls={
+        'Bug Tracker': 'https://github.com/savaij/astrodetection/issues',
+    },
+    license='MIT',
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    python_requires='>=3.7',
+    python_requires='>=3.8',
+    install_requires=[
+        'pandas',
+        'networkx',
+        'tqdm',
+        'demoji',
+        'requests',
+        'polyleven',
+        'ipysigma',
+        'numpy',
+    ],
+    extras_require={
+        'standard': [
+            'faiss-cpu',
+            'fasttext',
+            'gensim',
+        ],
+        'light': [
+            'scikit-learn',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+    ],
 )
