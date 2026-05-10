f991eb43e5bac2358b89af4eaca940afc57a6587
savaij <sdrogo.lover@gmail.com>
2026-05-10 11:42:22 +0200

    fix(setup): bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='astrodetection',
-    version='0.2.0',
+    version='0.2.1',
     description='A Python library for detecting astroturfing (coordinated inauthentic behavior) in social media posts.',
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
